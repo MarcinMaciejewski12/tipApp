df10f7a86fa7c92e56b8ec98e5fe80fbef4a7f74
Marcin <maciejewskim118@gmail.com>
2022-08-19 20:08:16 +0200

    update tip percent value

--- src/components/billSelector/BillCalculateStyle.tsx
+++ src/components/billSelector/BillCalculateStyle.tsx
@@ -47,16 +47,20 @@ export const TipSelector = styled.section`
         color: #575757;
         display: flex;
     }
-    & div {
+    & button {
         width: 80px;
         height: 40px;
         font-size: 25px;
         background:  hsl(183, 100%, 23.52941176470588%);
+        color: white;
         margin: 4px;
         display: flex;
+        align-items: center;
+        font-family: 'Poppins', sans-serif;
         justify-content: space-around;
         float: left;
         font-weight: 800;
+        border: none;
         border-radius: 5px 5px 5px 5px;
         cursor: pointer;
     }
--- src/components/billSelector/PercentSelector.tsx
+++ src/components/billSelector/PercentSelector.tsx
@@ -1,15 +1,24 @@
 import { TipSelector } from "./BillCalculateStyle";
 
+const TipValue = {
+    fivePer: 1.055,
+    tenPer: 1.1,
+    fiveteenPer: 1.15,
+    twentyPer: 1.20,
+    twentyFivePer: 1.25,
+    fiftyPer: 1.5
+}
+
 const PercentSelector = () => {
     return  (
 <TipSelector>
     <label>Select Tip %</label>
-    <div>5%</div>
-    <div>10%</div>
-    <div>15%</div>
-    <div>20%</div>
-    <div>25%</div>
-    <div>50%</div>
+    <button value={TipValue.fivePer}>5%</button>
+    <button value={TipValue.tenPer}>10%</button>
+    <button value={TipValue.fiveteenPer}>15%</button>
+    <button value={TipValue.twentyPer}>20%</button>
+    <button value={TipValue.twentyFivePer}>25%</button>
+    <button value={TipValue.fiftyPer}>50%</button>
     <input type="number" placeholder="%"/>
 </TipSelector>
 )
